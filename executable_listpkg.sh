af06b27bf80d25be8b3313a93f002e853387c8d1
escape0707 <tothesong@gmail.com>
2023-08-06 14:59:09 +0900

    Update config

--- executable_listpkg.sh
+++ executable_listpkg.sh
@@ -1,3 +1,3 @@
-installed_groups=('gnome' 'fcitx5-im' 'texlive-most')
+installed_groups=('gnome' 'fcitx5-im')
 comm -23 <({ pacman -Qqtt; printf "%s\n" "${installed_groups[@]}"; } | sort) <(pacman -Qqg $(printf " %s" "${installed_groups[@]}") | sort)
 
